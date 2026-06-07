ccba954db6888e6ebd76c8bbd6a44f160a51c8ab
sk00t89 <mark.eriksson89@gmail.com>
2026-06-07 17:32:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,146 +28,238 @@ const state = {
     bonus: 0.02,
 };
 
-const roads = {
-    name: "Roads",
+const businessesObj = {
+  roads: {
+    name: 'Roads',
     amount: 1,
     earning: 2,
-    timecycle: 500,
+    timecycle: 1000,
     bot: false,
     cost: 1,
     adder: 2,
     intervalId: null,
     producing: false,
     timeoutId: null,
+    goal: 3,
+    startAmount: 1,
+    startEarning: 2,
+    startAdder: 2,
+    startTimecycle: 1000,
+    startCost: 1,
+    multiplier: 1,
+    goalsTest: [5, 10, 20, 30],
+
+
+
+    elements: {
+      status: roadStatus,
+      title: roadsNum,
+      maxBtn: roadBtn5,
+      cycleStatus: roadCycleStatus,
+      cycleBtn: roadCycleBtn,
+      botBtn: roadPoliBtn,
+      goal: roadGoal,
+    },
+  },
+
+};
+
+
+
+const updateState = (business) => {
+
+
+    navMoney.innerText = `You have $${Math.floor(state.money)}.`;
+    business.elements.status.innerText = `${business.adder}$ each cycle`;
+    business.elements.title.innerText = `${business.name}     ${business.amount}`;
+    business.elements.maxBtn.innerText = `Buy ${Math.floor(state.money / business.cost)}`;
+    business.elements.cycleStatus.innerText = `${business.timecycle / 1000} sec`;
+    business.elements.goal.innerText = `${business.goal}`;
+    
+};
+
+const goalTest = (business, num) => {
+  for (let i = 0; i < num; i++) {
+    if ( num === business.goalsTest[i+1]) {
+      business.goalsTest.splice(i + 1, 1);
+      business.goal = business.goalsTest[0];
+    }
+  }
 }
 
+const checkGoal = (business) => {
 
-const updateState = () => {
-    navMoney.innerText = `You have $${Math.floor(state.money)}.`;
-    roadStatus.innerText = `${roads.adder}$ each cycle`;
-    roadsNum.innerText = `Roads     ${roads.amount}`;
-    roadBtn5.innerText = `Buy ${Math.floor(state.money / roads.cost)}`
-};
 
-updateState();
-
-const buyBusiness = (business, num) => {
-    if (state.money < business.cost * num) {
-        console.log("You dont have enough money.");
+    if (business.goal >= 5 && business.amount >= 5) {
+        business.timecycle = Math.floor(business.timecycle / 2)
+        business.goal = 10;
+        console.log("Next goal " + business.goal);
+        updateState(business);
         return;
     }
-    state.money -= business.cost * num;
-    business.amount += num;
-    business.adder += Math.floor(business.amount * 2);
-    business.cost += Math.floor(business.cost * 1.8);
-    console.log(`Cost for roads ${business.cost * num}`);
-    updateState();
+    if (business.goal >= 3 && business.amount >= 3) {
+        business.timecycle = business.timecycle / 2;
+        business.goal = 5;
+        console.log("goal met!" + business.goal);
+        updateState(business);
+        return;
+    }
 };
+
+
+const buyBusiness = (business, num) => {
+
+  for (let i = 0; i < num; i++) {
+
+    if (state.money < business.cost) {
+      console.log('You dont have enough money.');
+      break;
+    }
+    state.money -= business.cost;
+    business.amount += 1;
+    business.adder += Math.floor(business.amount * 1.5);
+    business.cost += Math.floor(business.cost * 1.8);
+
+    checkGoal(business);
+  }
+
+
+    updateState(business);
+
+};
+
+// Buttons
 
 roadBtn1.addEventListener("click", () => {
     const value = Number(roadBtn1.value);
-    buyBusiness(roads, value);
+    buyBusiness(businessesObj.roads, value);
 
 });
 roadBtn2.addEventListener("click", () => {
     const value = Number(roadBtn2.value);
-    buyBusiness(roads, value);
+    buyBusiness(businessesObj.roads, value);
 
 });
 roadBtn3.addEventListener("click", () => {
     const value = Number(roadBtn3.value);
-    buyBusiness(roads, value);
+    buyBusiness(businessesObj.roads, value);
 
 });
 roadBtn4.addEventListener("click", () => {
     const value = Number(roadBtn4.value);
-    buyBusiness(roads, value);
+    buyBusiness(businessesObj.roads, value);
 
 });
 roadBtn5.addEventListener("click", () => {
-    let maxBuys = Math.floor(state.money / roads.cost);
+    let maxBuys = Math.floor(state.money / businessesObj.roads.cost);
     console.log(maxBuys);
     if (maxBuys < 1 ) {
         console.log("Maxknapp är för lite");
         return;
     }
-    buyBusiness(roads, maxBuys);
+    buyBusiness(businessesObj.roads, maxBuys);
     console.log(`${maxBuys} roads bought.`)
 });
 
+
+// Payout funktion
 const payout = (business) => {
-    const money = state.money += business.adder;
-    money;
-    console.log(`${business.adder} added funds`)
-    console.log(business.adder)
-    updateState();
+
+  const payoutAmount = business.adder * business.multiplier;
+
+  state.money += payoutAmount;
+
+  updateState(business);
 };
 
-const roadCycle = () => {
-    if (roads.producing) {
-        console.log("You already have a roadcycle")
-        return;
-    }
+// Generell cykelfunktion
 
-    roadCycleBtn.disabled = true;
-    roads.producing = true;
+const runCycle = (business) => {
 
-    console.log("Roadcycle started");
+  if(business.producing) {
+    console.log(`Your ${business.name} is already running`);
+    return;
+  }
 
-    roads.timeoutId = setTimeout(() => {
-        payout(roads);
+  business.elements.cycleBtn.disabled = true;
+  business.producing = true;
 
-        roads.producing = false;
-        roadCycleBtn.disabled = false;
+  business.timeoutId = setTimeout(() => {
+    payout(business);
 
-        console.log("Roadcycle finshed");
+    business.producing = false;
+    business.elements.cycleBtn.disabled = false;
 
-    } , roads.timecycle);
+    console.log(`${business.name} finshed`);
+  }, business.timecycle)
 
 };
+
 
 roadCycleBtn.addEventListener("click", () => {
-    roadCycle();
+    runCycle(businessesObj.roads);
 });
 
+// Bot-funktion
+const hireBot = (business, cost) => {
+
+  if (business.bot) {
+    console.log(`You already have a bot.`);
+    return;
+  }
+  if (cost > state.money) {
+    console.log(`You dont have enough money for a bot.`);
+    return;
+  }
+
+  business.intervalId = setInterval(() =>
+    payout(business), business.timecycle,
+  );
+
+  business.bot = true;
+  state.money -= cost;
+  business.elements.botBtn.disabled = true;
+
+  console.log(`${business} bot started.`);
+  updateState(business);
+
+
+};
+
+// Politician btn
 roadPoliBtn.addEventListener("click", () => {
     const cost = Number(roadPoliBtn.value);
 
-    if (roads.bot === true) {
-        console.log("Du har redan bot");
-        return;
-    } if (cost > state.money) {
-        console.log("för lite pengar för politiker")
-        return;
-    }
-        roads.intervalId = setInterval(() => payout(roads), roads.timecycle);
-
-        roads.bot = true;
-        state.money -= cost;
-        roadCycleStatus.innerText = `Cycle: ${roads.timecycle / 1000} sec`
-        roadPoliBtn.disabled = true;
-
-        updateState();
-
+    hireBot(businessesObj.roads, cost);
 });
 
+
+// Reset Btn
 resetBtn.addEventListener("click", () => {
-    state.money = 1;
-    roads.producing = false;
-    clearTimeout(roads.timeoutId);
-    roads.timeoutId = null;
 
-    roads.amount = 1;
-    roads.cost = 1;
-    roads.adder = 2;
-    roads.bot = false;
+  state.money = 1;
 
-    clearInterval(roads.intervalId);
-    roads.intervalId = null;
+  Object.values(businessesObj).forEach((b) => {
+    clearInterval(b.intervalId);
+    clearTimeout(b.timeoutId);
 
-    roadCycleBtn.disabled = false;
-    roadPoliBtn.disabled = false;
-    roadCycleStatus.innerText = "Cycle: manual";
+    b.amount = b.startAmount;
+    b.cost = b.startCost;
+    b.adder = b.startAdder;
+    b.timecycle = b.startTimecycle;
 
-    updateState();
-})
+    b.multiplier = 1;
+    b.bot = false;
+    b.producing = false;
+    b.intervalId = null;
+    b.timeoutId = null;
+    b.goal = 3;
+
+    b.elements.status.innerText = "Cycle: manual";
+    b.elements.cycleBtn.disabled = false;
+    b.elements.botBtn.disabled = false;
+
+    updateState(b);
+  });
+
+});
